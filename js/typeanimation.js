31c00fe99a433234cf2e5c14be513706421d422f
divne <sirfoyx@gmail.com>
2023-10-22 20:51:34 +0100

    first push

---
+++ js/typeanimation.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('.typed', {
+         strings: ["Today.", "Fast.","Journey." ],
+         typeSpeed: 80,
+         loop: true,
+         backSpeed: 40,
+         backDelay: 3000
+         });
